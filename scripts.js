ad5e3646ce24a5d9de110d9c4957543a8111d13b
elfirl <mike.bilter@gmail.com>
2015-11-18 19:14:21 -0500

    Updated the logic so it would account for a string input as well as a date range.

--- scripts.js
+++ scripts.js
@@ -7,14 +7,17 @@ $(document).ready(function() {
 
 		// Checks to see if the click event is triggered from a button with id of submit-button
 		$("#submit-button").on("click", function(event) {
-			// Prevents the default behavior of the submit event.
 			event.preventDefault();
-			// Creates a variable and sets its value to the input of an element with id birth-year
-			var birthYear = $("#birth-year").val();
-			// Creates a variable and sets its value to 2015 minus the value of the variable birthYear
-			var age = 2015 - birthYear;
-			// Finds an element with the id calculated-age, and adds to the HTML a string with the variable age in the middle.
-			$("#calculated-age").html("You are " + age + " years old.");
+
+			var birthYear = parseInt($("#birth-year").val());
+
+			if ((1885 < birthYear) && (birthYear < 2015)) {
+				var age = 2015 - birthYear;
+				$("#calculated-age").html("You are " + age + " years old.");
+				} else {
+					$("#calculated-age").html("Yo dawg - Enter a valid year between 1885 and 2015.");
+			}
+			
 		});
 
 });
